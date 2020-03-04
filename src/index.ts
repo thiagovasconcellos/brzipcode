d33be3f3f74c3449683338bb91c4a8ba568c7960
thiagovasconcellos <thiago.vasconcellos@cbyk.com.br>
2020-03-04 10:08:02 -0300

    feature: Get address from zip code added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,82 @@
 const isValidCep = require('@brazilian-utils/is-valid-cep');
-const zipCodeAndStates = require('./data/ZipCodeAndStates');
+const axios = require('axios').default;
 
 interface ZipCodeObject {
   state: string;
   range: { start: number; finish: number }[];
 }
 
+interface ZipCodeAddress {
+  zipCode: string;
+  address: string;
+  complement: string;
+  district: string;
+  city: string;
+  state: string;
+  ibge: string;
+  gia: string;
+}
+
+const zipCodeAndStates = {
+  SP: { state: 'São Paulo', range: [{ start: 11000000, finish: 19999999 }] },
+  RJ: { state: 'Rio de Janeiro', range: [{ start: 20000000, finish: 28999999 }] },
+  ES: { state: 'Espírito Santo', range: [{ start: 29000000, finish: 29999999 }] },
+  MG: { state: 'Minas Gerais', range: [{ start: 30000000, finish: 39999999 }] },
+  BA: { state: 'Bahia', range: [{ start: 40000000, finish: 48999999 }] },
+  SE: { state: 'Sergipe', range: [{ start: 49000000, finish: 49999999 }] },
+  PE: { state: 'Pernambuco', range: [{ start: 50000000, finish: 56999999 }] },
+  AL: { state: 'Alagoas', range: [{ start: 57000000, finish: 57999999 }] },
+  PB: { state: 'Paraíba', range: [{ start: 58000000, finish: 58999999 }] },
+  RN: { state: 'Rio Grande do Norte', range: [{ start: 59000000, finish: 59999999 }] },
+  CE: { state: 'Ceará', range: [{ start: 60000000, finish: 63999999 }] },
+  PI: { state: 'Piauí', range: [{ start: 64000000, finish: 64999999 }] },
+  MA: { state: 'Maranhão', range: [{ start: 65000000, finish: 65999999 }] },
+  PA: { state: 'Pará', range: [{ start: 66000000, finish: 68899999 }] },
+  AP: { state: 'Amapá', range: [{ start: 68900000, finish: 68999999 }] },
+  AM: {
+    state: 'Amazonas',
+    range: [
+      { start: 69000000, finish: 69299999 },
+      { start: 69400000, finish: 69899999 },
+    ],
+  },
+  AC: { state: 'Acre', range: [{ start: 69900000, finish: 69999999 }] },
+  RR: { state: 'Roraima', range: [{ start: 69300000, finish: 69399999 }] },
+  DF: {
+    state: 'Distrito Federal',
+    range: [
+      { start: 70000000, finish: 72799999 },
+      { start: 73000000, finish: 73699999 },
+    ],
+  },
+  GO: {
+    state: 'Goiás',
+    range: [
+      { start: 72800000, finish: 72999999 },
+      { start: 73700000, finish: 76799999 },
+    ],
+  },
+  RO: {
+    state: 'Rondônia',
+    range: [
+      { start: 76800000, finish: 76999999 },
+      { start: 78900000, finish: 78999999 },
+    ],
+  },
+  TO: { state: 'Tocantins', range: [{ start: 77000000, finish: 77999999 }] },
+  MT: { state: 'Mato Grosso', range: [{ start: 78000000, finish: 78899999 }] },
+  MS: { state: 'Mato Grosso do Sul', range: [{ start: 79000000, finish: 79999999 }] },
+  PR: { state: 'Paraná', range: [{ start: 80000000, finish: 87999999 }] },
+  SC: { state: 'Santa Catarina', range: [{ start: 88000000, finish: 89999999 }] },
+  RS: { state: 'Rio Grande do Sul', range: [{ start: 90000000, finish: 99999999 }] },
+};
+
 /**
  * This method takes an state as argument and return it's ZipCode
  * @param state  Use the abbreviation. For example "SP" for São Paulo, "RJ" for Rio de Janeiro.
  */
 function ZipCodesFromGivenState(state: string): ZipCodeObject {
-  return zipCodeAndStates[state];
+  return zipCodeAndStates[state] || `${state} is not an valid brazilian state.`;
 }
 
 /**
@@ -114,4 +179,32 @@ function GenerateRandomZipCode(state: string): number {
   );
 }
 
-export = { ZipCodesFromGivenState, StateFromZipCode, isZipCodeInState, GenerateRandomZipCode };
+/**
+ * This method returns a JSON with a full address of a given ZipCode.
+ * @param zipCode  Use number format, without minus operator and without 0 on the left. For example: 1001001 for "Praça da Sé (01001-001).
+ */
+async function GetAddressFromZipCode(zipCode: number): Promise<ZipCodeAddress> {
+  let zeroZipCode;
+  if (zipCode.toString().length === 7) {
+    zeroZipCode = `0${zipCode}`;
+  }
+  try {
+    const response = await axios.get(`https://viacep.com.br/ws/${zeroZipCode || zipCode.toString()}/json/`);
+    if (response.data.erro) throw new Error('Could not find a valid address with the given ZipCode');
+
+    return {
+      zipCode: response.data.cep,
+      address: response.data.logradouro,
+      complement: response.data.complemento,
+      district: response.data.bairro,
+      city: response.data.localidade,
+      state: response.data.uf,
+      ibge: response.data.ibge,
+      gia: response.data.gia,
+    };
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export = { ZipCodesFromGivenState, StateFromZipCode, isZipCodeInState, GenerateRandomZipCode, GetAddressFromZipCode };
